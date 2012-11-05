173c2defddd49fcfcf29f90d4c2d087d0a4341b9
JP Richardson <jprichardson@gmail.com>
2012-11-04 23:29:06 -0600

    Change slug path from public/articles/ to public/ on rock personal_blog.

--- test/site.test.js
+++ test/site.test.js
@@ -68,13 +68,13 @@ describe('Site', function() {
         });
       });
     });
-    
+
     
     describe('- buildAllArticles()', function() {
       it('should build all of the articles into a build dir with default options', function(done) {
         var buildDir = path.join(TEST_DIR, 'public')
           , articleDir = path.join(TEST_DIR, 'articles')
-          , buildArticleDir = path.join(buildDir, 'articles')
+          , buildArticleDir = path.join(buildDir, '')
           , t1 = 'The Fall of the Roman Empire'
           , t2 = 'Applications of Austrian Economics'
           , t3 = "Napoleon's Conquests"
@@ -84,7 +84,6 @@ describe('Site', function() {
           , o1 = path.join(buildArticleDir, s1 + '.html')
           , o2 = path.join(buildArticleDir, s2 + '.html')
           , o3 = path.join(buildArticleDir, s3 + '.html')
-          , data = "{{article.title}}\n===============\n\nBlah blah blah"
           , site = Site.create(TEST_DIR, 'personal_blog');
         
         site.generateSkeleton(function(err) {
